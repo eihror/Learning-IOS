4b5dce9d7cfb677a4495c80b67f653cca717beb1
Igor Melo <igor_gmelo@hotmail.com>
2019-08-20 03:26:13 -0300

    Adding I Am Poor project and Start Dicee project

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Igor Melo on 20/08/19.
+//  Copyright © 2019 Igor Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
