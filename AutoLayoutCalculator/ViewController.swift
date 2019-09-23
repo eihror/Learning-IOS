d9f499ff8b9278ac6ca52cb44cc306303bf3f043
Igor Melo <igor.melo@mesainc.com.br>
2019-09-23 13:29:05 -0300

    Learning Auto Layout

---
+++ AutoLayoutCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutCalculator
+//
+//  Created by Igor Melo on 23/09/19.
+//  Copyright © 2019 Igor Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
